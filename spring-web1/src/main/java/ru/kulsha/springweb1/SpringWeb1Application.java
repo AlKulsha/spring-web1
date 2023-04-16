db2dca81934f62e1373b02745e867c7eaec13110
AlKulsha <alenabat18@gmail.com>
2023-04-16 15:54:21 +0300

    Initial commit

---
+++ spring-web1/src/main/java/ru/kulsha/springweb1/SpringWeb1Application.java
@@ -0,0 +1,14 @@
+package ru.kulsha.springweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb1Application {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringWeb1Application.class, args);
+	}
+
+}
